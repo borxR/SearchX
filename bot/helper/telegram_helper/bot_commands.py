01d8a24efb3d39c698994b25eacfa4aa5a71ae86
borxR <94912459+borxR@users.noreply.github.com>
2022-05-12 23:50:01 +0530

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -1,15 +1,15 @@
 class _BotCommands:
     def __init__(self):
         self.StartCommand = 'start'
-        self.ListCommand = 'search'
-        self.CloneCommand = 'clone'
-        self.CountCommand = 'count'
-        self.DeleteCommand = 'del'
-        self.AuthUsersCommand = 'users'
-        self.ShellCommand = 'shell'
-        self.LogCommand = 'log'
-        self.HelpCommand = 'help'
-        self.TestCommand = 'test'
+        self.ListCommand = 'search1'
+        self.CloneCommand = 'clone1'
+        self.CountCommand = 'count1'
+        self.DeleteCommand = 'del1'
+        self.AuthUsersCommand = 'users1'
+        self.ShellCommand = 'shell1'
+        self.LogCommand = 'log1'
+        self.HelpCommand = 'help1'
+        self.TestCommand = 'test1'
         
 
 BotCommands = _BotCommands()
